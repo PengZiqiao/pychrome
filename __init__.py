a2177f7238044ecd1e3d37ea22f1e12ffd6e7e0b
Peng Ziqiao <xfyx.student@sina.com>
2018-06-21 09:38:04 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,10 +2,13 @@ from os import system as cmd
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 
-cmd('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --remote-debugging-port=8888')
+PORT = 8888
+DRIVER_PATH = 'C:/webdrivers/chromedriver.exe'
+CHROME_PATH = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+
+cmd(f'"{CHROME_PATH}" --remote-debugging-port={PORT}')
 
 options = ChromeOptions()
-options.debugger_address = '127.0.0.1:8888'
-executable_path = 'c:/webdrivers/chromedriver.exe'
+options.debugger_address = f'127.0.0.1:{PORT}'
 driver = webdriver.Chrome(
-    executable_path=executable_path, chrome_options=options)
+    executable_path=DRIVER_PATH, chrome_options=options)
